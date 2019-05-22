6bf8af0c4f6a2ec632fb9842008a8f9c409d5ce7
sissini <1018762774@qq.com>
2019-05-22 19:30:10 +0200

    show the image
    read and show image
    wenn i type 'esc' then quit
    if i type 's' on the keyboard then save this image at a new adress

---
+++ test.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+#import numpy as np
+#import matplotlib.pyplot as plt 
+
+
+
+img = cv.imread('jjlin.jpg',0)
+# IMREAD_GRAYCOLOR = 0
+# IMREAD_COLOR =1
+# IMREAD_UNCHANGED = -1
+
+##显示图片##
+#cv.namedWindow('w',cv.WINDOW_NORMAL)	## 改变窗口大小
+cv.imshow('w', img)
+key = cv.waitKey(0)
+if key == 27:	#esc exit
+	cv.destroyAllWindows()
+if key == ord('s'):
+	cv.imwrite('jj_neu.jpg',img)
+	cv.destroyAllWindows()
